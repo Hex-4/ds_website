272b45ee73585256bfba7379b4ef45ae03832de5
Hex4 <121138331+Hex-4@users.noreply.github.com>
2022-12-20 17:19:15 -0700

    uhh

---
+++ src/pages/home.js
@@ -0,0 +1,17 @@
+
+export default function Home() {
+    return (
+        <div id="page" className="fixed top-0 left-16 float-right overflow-scroll-y bg-primary">
+            <div className="from-blue-500 to-green-500 p-40 w-screen transiton-all bg-gradient-to-bl">
+                <img src="https://ibb.co/VmhV072"></img>
+                <h1 className="text-white text-7xl text-center font-black">Welcome to the <b className="text-secondary">Dimaverse.</b></h1>
+            </div>
+            <div>
+                <h2></h2>
+            </div>
+            
+            
+            
+        </div>
+    )
+}
