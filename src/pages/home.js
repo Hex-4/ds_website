d6f97e29b1705bd4f8907c51cbc26bfd9504ef5b
Hex-4 <121138331+Hex-4@users.noreply.github.com>
2022-12-23 17:23:24 +0000

    fix scroll

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,7 @@ import wordmark from "./img/wordmark-logo.png"
 
 export default function Home() {
     return (
-        <div id="page" className="fixed top-0 left-16 float-right overflow-scroll-y bg-primary h-screen z-0">
+        <div id="page" className="fixed top-0 left-16 float-right overflow-scroll bg-primary h-screen z-0">
             <div className="from-blue-500 to-green-500 p-40 w-screen transiton-all bg-gradient-to-bl">
                 <img src={wordmark} class="w-64 h-auto text-center self-center mx-auto"></img>
                 <h1 className="text-white text-7xl text-center font-black">Welcome to the <b className="text-secondary">Dimaverse.</b></h1>
